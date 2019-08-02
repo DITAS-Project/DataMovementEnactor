3e658daaa5d8402d2f32b33bc998c24e00248fbc
Bogdan Despotov <despotov@gmail.com>
2019-08-02 15:53:16 +0300

    Adding the Flask main file to test the multiprocessing with the DME agent

---
+++ __main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process
+
+import connexion
+from .check_database import dme_agent
+from swagger_server import encoder
+
+
+def main():
+    app = connexion.App(__name__, specification_dir='./swagger/')
+    app.app.json_encoder = encoder.JSONEncoder
+    p = Process(target=dme_agent)
+    app.add_api('swagger.yaml', arguments={'title': 'Data Movement Enactor'})
+    p.start()
+    app.run(port=8080, use_reloader=False)
+    p.join()
+
+
+if __name__ == '__main__':
+    main()
--- check_database.py
+++ check_database.py
@@ -1,3 +1,5 @@
+import time
+
 import MySQLdb
 from .dal_client import start_data_movement
 
@@ -25,68 +27,69 @@ def compile_sql_query(operation, table_name, insert_order, values, where_cond=No
     return query
 
 
-try:
-    db = MySQLdb.connect(user='symmetricds', passwd='',
-                         db='hospital', host='127.0.0.1', port=5601)
-except MySQLdb.Error as e:
-    raise e
+def dme_agent():
+    try:
+        db = MySQLdb.connect(user='symmetricds', passwd='',
+                             db='hospital', host='127.0.0.1', port=5601)
+    except MySQLdb.Error as e:
+        raise e
 
-db.autocommit(True)
-cursor = db.cursor()
-cursor.execute('show tables')
-tables = cursor.fetchall()
+    db.autocommit(True)
+    cursor = db.cursor()
+    cursor.execute('show tables')
+    tables = cursor.fetchall()
 
-sym_data_table = False
-table_names = []
+    sym_data_table = False
+    table_names = []
 
-for table in tables:
-    if not table[0].startswith('sym'):
-        table_names.append(table[0])
-    elif table[0] == 'sym_data':
-        sym_data_table = True
+    for table in tables:
+        if not table[0].startswith('sym'):
+            table_names.append(table[0])
+        elif table[0] == 'sym_data':
+            sym_data_table = True
 
-if not sym_data_table:
-    raise Exception('sym_data table not found in database')
+    if not sym_data_table:
+        raise Exception('sym_data table not found in database')
 
-last_id = 0
-while True:
-    cursor.execute("SELECT * FROM sym_data WHERE data_id = (SELECT MAX(data_id) FROM sym_data)")
-    result = cursor.fetchone()
-    if result[1] in table_names and result[0] != last_id:
-        last_id = result[0]
-        cursor.execute("describe {}".format(result[1]))
-        res = cursor.fetchall()
-        table_cols = []
-        primary_keys = []
+    last_id = 0
+    while True:
+        cursor.execute("SELECT * FROM sym_data WHERE data_id = (SELECT MAX(data_id) FROM sym_data)")
+        result = cursor.fetchone()
+        if result[1] in table_names and result[0] != last_id:
+            last_id = result[0]
+            cursor.execute("describe {}".format(result[1]))
+            res = cursor.fetchall()
+            table_cols = []
+            primary_keys = []
 
-        for r in res:
-            if r[3] != 'PRI':
-                table_cols.append(r[0])
-            else:
-                table_cols.insert(0, r[0])
-                primary_keys.append(r[0])
+            for r in res:
+                if r[3] != 'PRI':
+                    table_cols.append(r[0])
+                else:
+                    table_cols.insert(0, r[0])
+                    primary_keys.append(r[0])
 
-        where_cond = None
-        if result[2] == 'U' or result[2] == 'D':
-            pk_data = result[4].split(',')
-            if len(pk_data) != len(primary_keys):
-                raise Exception('pk_data data does not match primary key number')
-            elif len(pk_data) > 1:
-                zipped = zip(primary_keys, pk_data)
-                where_values = []
-                for i in zipped:
-                    where_values.append('{} = {}'.format(i[0], i[1]))
-                where_cond = 'AND'.join(x for x in where_values)
-            elif len(pk_data) == 1:
-                where_cond = '{} = {}'.format(primary_keys[0], pk_data[0])
+            where_cond = None
+            if result[2] == 'U' or result[2] == 'D':
+                pk_data = result[4].split(',')
+                if len(pk_data) != len(primary_keys):
+                    raise Exception('pk_data data does not match primary key number')
+                elif len(pk_data) > 1:
+                    zipped = zip(primary_keys, pk_data)
+                    where_values = []
+                    for i in zipped:
+                        where_values.append('{} = {}'.format(i[0], i[1]))
+                    where_cond = 'AND'.join(x for x in where_values)
+                elif len(pk_data) == 1:
+                    where_cond = '{} = {}'.format(primary_keys[0], pk_data[0])
 
-        sql_query = compile_sql_query(operation=result[2], table_name=result[1],
-                                      insert_order=table_cols, values=result[3], where_cond=where_cond)
-        print(sql_query)
+            sql_query = compile_sql_query(operation=result[2], table_name=result[1],
+                                          insert_order=table_cols, values=result[3], where_cond=where_cond)
+            print(sql_query)
 
-        try:
-            start_data_movement(query=sql_query, path='/test')
-        except Exception as e:
-            print('Failed sending gRPC call: {}'.format(e))
+            try:
+                start_data_movement(query=sql_query, path='/test')
+            except Exception as e:
+                print('Failed sending gRPC call: {}'.format(e))
 
-
+            time.sleep(1)
